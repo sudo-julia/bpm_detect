132c17e8a0c81f5bf63bb1f9e5c464b3183b4833
sudo-julia <jlearning@tuta.io>
2021-05-13 11:44:37 -0700

    get_downloads_dir complete

--- bpm_detect/get_downloads_dir.py
+++ bpm_detect/get_downloads_dir.py
@@ -1,10 +1,13 @@
 """get the user's downloads directory, independent of OS"""
 # pylint: disable=C0415, E0401
+from __future__ import annotations
 import os
+from pathlib import Path
 
 
 def get_downloads_dir() -> str:
     """get the download path"""
+    # get windows downloads path
     name: str = os.name
     if name == "nt":
         import winreg
@@ -12,9 +15,15 @@ def get_downloads_dir() -> str:
         sub_key: str = (
             r"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders"
         )
-        downloads_guid = "{374DE290-123F-4565-9164-39C4925E467B}"
+        downloads_guid: str = "{374DE290-123F-4565-9164-39C4925E467B}"
         with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
-            location = winreg.QueryValueEx(key, downloads_guid)[0]
-        return location
-    # TODO (jam) see if "Downloads" or "downloads" is in use, check XDG_DOWNLOAD_DIR
-    return os.path.join(os.path.expanduser("~"), "downloads")
+            return winreg.QueryValueEx(key, downloads_guid)[0]
+
+    # get unix downloads path
+    location: str | None
+    if not (location := os.environ.get("XDG_DOWNLOAD_DIR")):
+        if Path("~/downloads").expanduser().exists():
+            location = str(Path("~/downloads").expanduser())
+        else:
+            location = str(Path("~/Downloads").expanduser())
+    return location
